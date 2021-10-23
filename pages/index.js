09d7f2bf3ab21030dc5fa76b4b891dc29ed3d869
jamalpro90 <jamalpro90@gmail.com>
2021-10-23 16:00:12 +0700

    merubah posisi fixed ke navbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Hero from '../components/Hero';
 import Layout from '../components/Layout';
+import PopulerFoods from '../components/PopulerFoods';
 
 export default function Home() {
   return (
     <Layout title="Home">
       <Hero />
+      <PopulerFoods />
     </Layout>
   );
 }
