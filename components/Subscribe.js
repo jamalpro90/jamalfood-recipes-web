14850216fd2d92ad4a91fdfb2c09d92c84025c67
jamalpro90 <jamalpro90@gmail.com>
2021-10-26 00:17:41 +0700

    perbaiki subscribe

---
+++ components/Subscribe.js
@@ -0,0 +1,16 @@
+function Subscribe() {
+  return (
+    <div className="subscribe-container">
+      <div className="subscribe">
+        <h3>Langganan untuk mendapatkan resep tiap minggu</h3>
+        <div className="input-box">
+          <i className="far fa-envelope"></i>
+          <input type="text" placeholder="Langganan" />
+          <button>Langganan</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Subscribe;
