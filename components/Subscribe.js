a44b63ae775556ecfa171ecf1aab86f5b977a882
jamal <you@example.com>
2022-02-03 15:22:54 +0700

    ganti component Subscribe

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -7,13 +7,19 @@ function Subscribe() {
     >
       <div className="subscribe">
         <h3>Subscribe to get recipes every week</h3>
-        <div className="input-box">
+        {/* <div className="input-box">
           <i className="far fa-envelope"></i>
           <input type="text" placeholder="Subscribe" />
-        </div>
-        <div className="btn-container" onClick={() => alert("Not Available")}>
-          <button>Subscribe</button>
-        </div>
+        </div> */}
+        <a
+          href="https://www.youtube.com/c/samthecookingguy?sub_confirmation=1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="btn-container">
+            <button>Subscribe</button>
+          </div>
+        </a>
       </div>
     </div>
   );
